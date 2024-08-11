9cb41de60aef3896b815a46d2a1066f9287dbad0
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2024-08-11 21:18:12 +0530

    update

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import heroImg from '../assets/hero.webp'
+import { FaCircleArrowRight } from "react-icons/fa6";
 
 const Hero = () => {
   return (
     <section id='home' className='bg-heroBg text-white flex items-center pt-28 md:h-screen'>
-      <div>
-        <div className='md:w-1/2'>left side</div>
+      <div className='container mx-auto flex flex-col md:flex-row items-center justify-between p-8
+      overflow-y-hidden gap-12 h-full'>
+        <div className='md:w-1/2'>
+           <h1 className='text-4xl font-secondary font-bold mb-4 md:w-3/5 leading-snug'>Start Your Journey to Mental Wellness</h1>
+           <p className='text-lg mb-12 md:pr-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas aperiam, minus voluptatem minima reprehenderit,
+             amet, vero dolore ratione aliquam omnis officia deserunt hic ipsa cupiditate impedit repellendus quos excepturi officiis.</p>
+             <button className='bg-primary text-white py-3.5 px-8 font-medium rounded-md hover:bg-primary/90 '>
+              <a href='#contact' className='flex gap-1 items-center'>
+                <span>Get Started</span>
+                <FaCircleArrowRight/>
+              </a>
+             </button>
+        </div>
 
-        <div className='md:w-1/2 h-full'></div>
+        <div className='md:w-1/2 h-full '>
+         <img src={heroImg} alt='hero image' className='w-full object-cover'/>
+        </div>
       </div>
     </section>
   )
--- src/component/Service.jsx
+++ src/component/Service.jsx
@@ -1,10 +1,117 @@
-
 import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import serviceImg1 from "../assets/service1.webp"
+import serviceImg2 from "../assets/service2.webp"
+import serviceImg3 from "../assets/service3.webp"
+import serviceImg4 from "../assets/service4.webp"
 
-const Service = () => {
+
+const Services = () => {
   return (
-    <div id="services" className='h-screen bg-green-700'>Services</div>
+    <div id='services' className='bg-[#f7f8fc]'>
+        <div className='pt-28 px-4 container mx-auto'>
+            <div className='text-center space-y-5'>
+                <h2 className='text-4xl font-bold font-secondary text-heroBg'>What can we Do Together</h2>
+                <p className='md:w-1/2 mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui excepturi sequi suscipit sint eligendi sit alias,
+                     quod expedita inventore, accusamus totam eos nihil in quia nesciunt? Rem fuga ex eius.</p>
+            </div>
+
+            <div className='py-12 md:w-4/5 mx-auto'>
+            <Tabs>
+    <TabList className="flex flex-wrap justify-between items-center md:gap-8 gap-4">
+      <Tab>Couple Counseling</Tab>
+      <Tab>Parenting Skills</Tab>
+      <Tab>Feeling Stuck</Tab>
+      <Tab>Self-Confidence</Tab>
+    </TabList>
+
+    <TabPanel>
+      <div className="flex flex-col md:flex-row gap-8 mt-8">
+      <div className='md:w-1/2 bg-white rounded-lg p-12 font-secondary'>
+        <h3 className='text-3x1 font-semibold text-primary mb-4'>Couple Counseling</h3>
+        <p className='mb-8'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, aspernatur nulla iusto, dicta porro fuga ullam 
+            explicabo natus molestiae soluta non eius adipisci vero obcaecati laudantium rerum accusamus distinctio nihil?</p>
+            <h4 className='text-x1 font-medium text-black mb-4'>Benefits</h4>
+            <ul className='list-disc list-inside space-y-3'>
+                <li>Understanding Relationship Dynamics</li>
+                <li>Understanding Relationship Dynamics</li>
+                <li>Understanding Relationship Dynamics</li>
+            </ul>
+      </div>
+
+      <div className='md:w-1/2'>
+        <img src={serviceImg1} alt='' className='w-full h-auto rounded-2x1 object-cover'/>
+      </div>
+      </div>
+    </TabPanel>
+
+    <TabPanel>
+      <div className="flex flex-col md:flex-row gap-8 mt-8">
+      <div className='md:w-1/2 bg-white rounded-lg p-12 font-secondary'>
+        <h3 className='text-3x1 font-semibold text-primary mb-4'>Parenting Skills</h3>
+        <p className='mb-8'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, aspernatur nulla iusto, dicta porro fuga ullam 
+            explicabo natus molestiae soluta non eius adipisci vero obcaecati laudantium rerum accusamus distinctio nihil?</p>
+            <h4 className='text-x1 font-medium text-black mb-4'>Benefits</h4>
+            <ul className='list-disc list-inside space-y-3'>
+                <li>Understanding Relationship Dynamics</li>
+                <li>Understanding Relationship Dynamics</li>
+                <li>Understanding Relationship Dynamics</li>
+            </ul>
+      </div>
+
+      <div className='md:w-1/2'>
+        <img src={serviceImg2} alt='' className='w-full h-auto rounded-2x1 object-cover'/>
+      </div>
+      </div>
+    </TabPanel>
+
+    <TabPanel>
+      <div className="flex flex-col md:flex-row gap-8 mt-8">
+      <div className='md:w-1/2 bg-white rounded-lg p-12 font-secondary'>
+        <h3 className='text-3x1 font-semibold text-primary mb-4'>Feeling Stuck</h3>
+        <p className='mb-8'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, aspernatur nulla iusto, dicta porro fuga ullam 
+            explicabo natus molestiae soluta non eius adipisci vero obcaecati laudantium rerum accusamus distinctio nihil?</p>
+            <h4 className='text-x1 font-medium text-black mb-4'>Benefits</h4>
+            <ul className='list-disc list-inside space-y-3'>
+                <li>Understanding Relationship Dynamics</li>
+                <li>Understanding Relationship Dynamics</li>
+                <li>Understanding Relationship Dynamics</li>
+            </ul>
+      </div>
+
+      <div className='md:w-1/2'>
+        <img src={serviceImg3} alt='' className='w-full h-auto rounded-2x1 object-cover'/>
+      </div>
+      </div>
+    </TabPanel>
+
+    
+    <TabPanel>
+      <div className="flex flex-col md:flex-row gap-8 mt-8">
+      <div className='md:w-1/2 bg-white rounded-lg p-12 font-secondary'>
+        <h3 className='text-3x1 font-semibold text-primary mb-4'>Self-Confidence</h3>
+        <p className='mb-8'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, aspernatur nulla iusto, dicta porro fuga ullam 
+            explicabo natus molestiae soluta non eius adipisci vero obcaecati laudantium rerum accusamus distinctio nihil?</p>
+            <h4 className='text-x1 font-medium text-black mb-4'>Benefits</h4>
+            <ul className='list-disc list-inside space-y-3'>
+                <li>Understanding Relationship Dynamics</li>
+                <li>Understanding Relationship Dynamics</li>
+                <li>Understanding Relationship Dynamics</li>
+            </ul>
+      </div>
+
+      <div className='md:w-1/2'>
+        <img src={serviceImg4} alt='' className='w-full h-auto rounded-2x1 object-cover'/>
+      </div>
+      </div>
+    </TabPanel>
+    
+    
+  </Tabs>
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default Service
+export default Services
